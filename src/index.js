5982ad2f8d3df1131f3a64c3ff1fa6407a997737
Ionescu Alex <alx.ionescu97@gmail.com>
2020-10-08 19:09:36 +0300

    Testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const Driver = require('./driver');
-const Puppeteer = require('./browsers/puppeteer');
+const Puppeteer = require('/src/browsers/puppeteer');
 
 class Wappalyzer {
   constructor(pageUrl, options) {
     // eslint-disable-next-line import/no-dynamic-require, global-require
-    const Browser = require(`./src/browsers`);
+    const Browser = require(`./browsers`);
 
     return new Driver(Browser, pageUrl, options);
   }
